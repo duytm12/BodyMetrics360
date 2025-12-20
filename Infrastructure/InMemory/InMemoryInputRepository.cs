449f6b11298d11255c48126ded0865ea921f5137
Minh Duy Truong <mtruon11@wgu.edu>
2025-12-20 07:43:01 -0600

    Create Infratructure layer - In Memory Repository

---
+++ Infrastructure/InMemory/InMemoryInputRepository.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.InMemory;
+
+public class InMemoryInputRepository : IInputRepository
+{
+    private readonly ConcurrentDictionary<int, Input> _inputs = new();
+    private int _nextId = 1;
+    private readonly object _lockObject = new();
+    public Task<Input> AddAsync(Input input)
+    {
+        lock (_lockObject)
+        {
+            input.Id = _nextId++;
+            input.CreatedAt = DateTime.UtcNow;
+            _inputs.TryAdd(input.Id, input);
+        }
+        return Task.FromResult(input);
+    }
+
+    public Task<Input?> GetLatestAsync()
+    {
+        var input = _inputs.Values.OrderByDescending(i => i.CreatedAt).FirstOrDefault();
+        return Task.FromResult<Input?>(input);
+    }
+
+    public Task UpdateAsync(Input input)
+    {
+        _inputs.AddOrUpdate(input.Id, input, (key, oldValue) => input);
+        return Task.CompletedTask;
+    }
+}
---
+++ Infrastructure/InMemory/InMemoryOutputRepository.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.InMemory;
+
+public class InMemoryOutputRepository : IOutputRepository
+{
+    private readonly ConcurrentDictionary<int, Output> _outputs = new();
+
+    // Mapping from InputId to OutputId
+    private readonly ConcurrentDictionary<int, int> _inputIdToOutputId = new();
+
+    private int _nextId = 1;
+    private readonly object _lockObject = new();
+
+    public Task<Output> AddAsync(Output output)
+    {
+        lock (_lockObject)
+        {
+            output.Id = _nextId++;
+            output.CalculatedAt = DateTime.UtcNow;
+            _outputs.TryAdd(output.Id, output);
+            _inputIdToOutputId.AddOrUpdate(output.InputId, output.Id, (key, oldValue) => output.Id);
+        }
+        return Task.FromResult(output);
+    }
+
+    public Task<Output?> GetByInputIdAsync(int inputId)
+    {
+        if (_inputIdToOutputId.TryGetValue(inputId, out var outputId))
+        {
+            _outputs.TryGetValue(outputId, out var output);
+            return Task.FromResult(output);
+        }
+        return Task.FromResult<Output?>(null);
+    }
+
+    public Task<Output?> GetLatestAsync()
+    {
+        var output = _outputs.Values.OrderByDescending(o => o.CalculatedAt).FirstOrDefault();
+        return Task.FromResult(output);
+    }
+
+    public Task UpdateAsync(Output output)
+    {
+        _outputs.AddOrUpdate(output.Id, output, (key, oldValue) => output);
+        _inputIdToOutputId.AddOrUpdate(output.InputId, output.Id, (key, oldValue) => output.Id);
+        return Task.CompletedTask;
+    }
+}
