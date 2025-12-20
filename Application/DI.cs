72b1c23179ea591c94b16fc7efb1f3359baba780
Minh Duy Truong <mtruon11@wgu.edu>
2025-12-20 08:31:40 -0600

    Create Dependency Injection for each layer

---
+++ Application/DI.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Interfaces;
+using Domain.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DI
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // Register Domain Services
+        services.AddScoped<IGetBMI, GetBMI>();
+        services.AddScoped<IGetBFP, GetBFP>();
+        services.AddScoped<IGetLBM, GetLBM>();
+        services.AddScoped<IGetWtHR, GetWtHR>();
+
+        // Register Application Use Cases
+        services.AddScoped<UseCases.CalculateBMIUseCase>();
+        services.AddScoped<UseCases.CalculateBFPUseCase>();
+        services.AddScoped<UseCases.CalculateLBMUseCase>();
+        services.AddScoped<UseCases.CalculateWtHRUseCase>();
+
+        return services;
+    }
+}
--- Infrastructure/DI.cs
+++ Infrastructure/DI.cs
@@ -1,7 +1,18 @@
-﻿namespace Infrastructure
+﻿using Domain.Interfaces;
+using Infrastructure.InMemory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DI
 {
-    public class DI
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        // Register Repository Implementations
+        services.AddSingleton<IInputRepository, InMemoryInputRepository>();
+        services.AddSingleton<IOutputRepository, InMemoryOutputRepository>();
+
+        return services;
 
     }
 }
