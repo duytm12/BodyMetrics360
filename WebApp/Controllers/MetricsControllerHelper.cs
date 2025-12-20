83bbecf23f7db419698adeaeca4021f919abfda4
Minh Duy Truong <mtruon11@wgu.edu>
2025-12-20 09:22:52 -0600

    Create Metrics Controller helper methods Mapping

---
+++ WebApp/Controllers/MetricsControllerHelper.cs
@@ -0,0 +1,64 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WebApp.Controllers;
+
+public static class MetricsControllerHelper
+{
+    public static CalculateBMIRequest MapToBMIRequest(Input? input)
+    {
+        var request = new CalculateBMIRequest();
+        if (input != null)
+        {
+            request.WeightLbs = input.WeightLbs > 0 ? input.WeightLbs : 0;
+            request.HeightInches = input.HeightInches > 0 ? input.HeightInches : 0;
+            request.Age = input.Age > 0 ? input.Age : 0;
+            request.Gender = input.Gender;
+            request.ActivityLevel = input.ActivityLevel;
+        }
+
+        return request;
+    }
+
+    public static CalculateBFPRequest MapToBFPRequest(Input? input)
+    {
+        var request = new CalculateBFPRequest();
+        if (input != null)
+        {
+            request.WaistInches = input.WaistInches > 0 ? input.WaistInches : 0;
+            request.NeckInches = input.NeckInches > 0 ? input.NeckInches : 0;
+            request.HipInches = input.HipInches > 0 ? input.HipInches : 0;
+            request.HeightInches = input.HeightInches > 0 ? input.HeightInches : 0;
+            request.Gender = input.Gender;
+        }
+        return request;
+    }
+
+    public static CalculateLBMRequest MapToLBMRequest(Input? input)
+    {
+        var request = new CalculateLBMRequest();
+        if (input != null)
+        {
+            request.WeightLbs = input.WeightLbs > 0 ? input.WeightLbs : 0;
+            request.HeightInches = input.HeightInches > 0 ? input.HeightInches : 0;
+            request.WaistInches = input.WaistInches > 0 ? input.WaistInches : 0;
+            request.NeckInches = input.NeckInches > 0 ? input.NeckInches : 0;
+            request.Gender = input.Gender;
+        }
+
+        return request;
+    }
+
+    public static CalculateWtHRRequest MapToWtHRRequest(Input? input)
+    {
+        var request = new CalculateWtHRRequest();
+        if (input != null)
+        {
+            request.WaistInches = input.WaistInches > 0 ? input.WaistInches : 0;
+            request.HeightInches = input.HeightInches > 0 ? input.HeightInches : 0;
+        }
+        return request;
+    }
+}
